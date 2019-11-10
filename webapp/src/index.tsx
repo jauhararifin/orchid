92b7b0b41db5d453650a138774e5bb85746c8ab9
Jauhar Arifin <jauhar.arifin@shopee.com>
2019-11-10 19:48:49 +0800

    add webapp ts react

---
+++ webapp/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
